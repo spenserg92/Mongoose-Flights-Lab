74ce6f28bb4aa2f8441c90860826740fbb34877d
spenserg92 <spenserg92@gmail.com>
2023-12-13 16:58:02 -0500

    adds flight model

---
+++ mongoose-flights/models/flight.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United'],
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'GRR', 'LAX', 'SAN'],
+        default: 'GRR'
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999,
+    },
+    departs: {
+        type: Date,
+        default: ()=>{
+            const yearFromNow = new Date();
+            yearFromNow.setFullYear(yearFromNow.getFullYear() + 1);
+            return yearFromNow;
+        }
+    }
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
