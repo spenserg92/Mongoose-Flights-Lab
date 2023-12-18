3c5b74e5611fb090e4bc711602c292fc6b7de963
spenserg92 <spenserg92@gmail.com>
2023-12-17 20:09:58 -0500

    adds a ticket Schema

---
+++ mongoose-flights/models/ticket.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ticketSchema = new Schema({
+    seat: {
+        type: String,
+        match: /[A-F][1-9]\d?/,
+    },
+    price: {
+        type: Number,
+        min: 0
+    },
+    flight: { type: mongoose.Types.ObjectId, ref: 'Flight'}
+})
+
